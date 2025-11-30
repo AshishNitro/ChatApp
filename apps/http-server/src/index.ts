5f434c0213edd620be2deb754eeb969af4376f59
Äshish sharma <ashishnitro5@gmail.com>
2025-11-30 06:52:37 +0530

    si,su APIs

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -3,21 +3,79 @@ import { middleware } from "./middleware";
 import jwt from "jsonwebtoken";
 import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/zodsc/types";
 import prismaClient from "@repo/database/client";
+import { JWT_SECRET } from "@repo/sigin/config";
 
 
 const app = express();
+app.use(express.json());
 
 
 
-app.post("/signup", (req, res)=>{
+app.post("/signup", async (req, res)=>{
 
-    const username = req.query.username;
-    const password = req.query.password;
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
+        console.log(parsedData.success);
+        res.json({
+            message: "Incorreect data",
+            
+        })
+        return;
+    }
+
+    try{
+        const user = await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                name: parsedData.data.name,
+                password: parsedData.data.password,
+            }
+        })
+        res.json({
+            userId: user.id})
+
+    } catch(err){
+        res.status(411).json({
+            message: "user aleady exists , try diffrent username "
+        })
+    }
+    
+    
 
     
 })
 
 app.post("/signin", (req , res)=>{
+
+
+    const parsedData = SignInSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message: "Incorrect data"
+        })
+        return;
+
+    }
+    const user = prismaClient.user.findFirst({
+        where:{
+            email: parsedData.data.username,
+            password: parsedData.data.password
+        }
+    })
+    if(!user){
+        res.status(403).json({
+            message:"NOt authorized"
+        })
+        return;
+
+    }
+
+    const token = jwt.sign({
+        userId:user?.id
+    }, JWT_SECRET);
+    res.json({
+        token
+    }) 
 
 })
 
@@ -36,4 +94,4 @@ app.get("/room/:slug", (req, res)=>{
 })
 
 
-app.listen(3000);
+app.listen(3001);
--- apps/http-server/src/middleware.ts
+++ apps/http-server/src/middleware.ts
@@ -1,10 +1,12 @@
 import { NextFunction , Request, Response} from "express";
 import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "@repo/sigin/config";
+//import { JWT_SECRET } from "@repo/backend-common/config";
  
 
 
 //will put into .env later on 
-const JWT_SECRET = process.env.JWT_SECRET || "1234567890";
+
 
 
 export function middleware(req: Request, res: Response, next: NextFunction){
